44dd79cba79843a630a0820e6211b6bf58985423
ashraful99-98 <ashrafulkamran98@gmail.com>
2023-10-31 15:05:56 +0600

    notification model create

---
+++ models/notificationModel.ts
@@ -0,0 +1,30 @@
+import mongoose,{Document,Model,Schema} from "mongoose";
+import { title } from "process";
+
+export interface INotification extends Document{
+    title: string;
+    message: string;
+    status: string;
+    userId: string
+}
+
+const notificationSchema = new Schema<INotification>({
+    title:{
+        type:String,
+        required: true,
+    },
+    message:{
+        type:String,
+        required: true,
+    },
+    status:{
+        type:String,
+        required: true,
+        default: "unread",
+    }
+
+},{timestamps: true});
+
+const NotificationModel : Model<INotification> = mongoose.model('Notification', notificationSchema);
+
+export default notificationSchema;
