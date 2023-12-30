55305a325f9cbe5458dc04ca7870493fb27e4841
mukhlesur1001 <mukhlesur1001@gmail.com>
2023-12-30 19:29:28 +0600

    user info added

--- utils/redis.ts
+++ utils/redis.ts
@@ -1,12 +1,12 @@
-import  { Redis } from 'ioredis';
-require('dotenv').config();
+require("dotenv").config();
+import { Redis } from "ioredis";
 
 const redisClient = () => {
-    if(process.env.REDIS_URL){
-        console.log('Redies connented');
-        return process.env.REDIS_URL;
-    }
-    throw new Error('Redies connection failed');
+  if (process.env.REDIS_URL) {
+    console.log("Redies connented");
+    return process.env.REDIS_URL;
+  }
+  throw new Error("Redies connection failed");
 };
 
 export const redis = new Redis(redisClient());
